e3b2cf5ac36cde96367eea8a8c9d1746efe4ef7f
Kristian <kristiansby@gmail.com>
2023-03-09 14:29:20 +0100

    python delta prefetcher done but not 100% error tested

--- py_demo.py
+++ py_demo.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class DPTEntry:
-   next_delta: int
+   prediction: int
    delatas: list
    LRU = 0
 
@@ -18,11 +18,27 @@ class Prefetcher:
       for i in range (0,n):
          self.DPTs.append([])
          for j in range (0,m):
-            self.DPTs[i].append(DPTEntry(None, [None] * (n+1)))
+            
+            deltas = []
+            
+            for k in range(0, i+1):
+               deltas.append(None)
+            
+            self.DPTs[i].append(DPTEntry(None, deltas))
+            
+            
+   def __uppdate_LRU(self, finds):
+      for i in range(0, self.n):
+         for j in range(0, self.n):
+            self.DPTs[i][j].LRU += 1
+            
+      for i, j in finds: 
+         self.DPTs[i][j].LRU = 0
+      
             
    #This function updates the delata 
    def __update_deltas(self, raw_delta_sequence, next_delta):
-      
+      print(f"raw_delta_sequence: {raw_delta_sequence}, next_delta: {next_delta}")
       #Exmple:
       
       #raw_delata_seqence = 1,2,4,8
@@ -36,39 +52,37 @@ class Prefetcher:
       for i in range(0, self.n):
          victem = 0
          
-         d_i = self.n - i - 1
-         
          delta_seqence = []
          
          #n = 4
-         # i = 0: b = [1],          append = 3,          diff = 2
-         # i = 1: b = [1, 2],       append = 2, 3,       diff = 1 
-         # i = 2: b = [1, 2, 3],    append = 1, 2, 3     diff = 0 
-         # i = 3: b = [1, 2, 3, 4], append = 0, 1, 2, 3  diff = -1
+         # i = 0: b = [0],          append = 3,          diff = 3  n - 1- i
+         # i = 1: b = [0, 1],       append = 2, 3,       diff = 2 
+         # i = 2: b = [0, 1, 2],    append = 1, 2, 3     diff = 1 
+         # i = 3: b = [0, 1, 2, 3], append = 0, 1, 2, 3  diff = 0
          
-         for b in range (1, i+2):
-            delta_seqence.append(raw_delta_sequence[b + self.n -2 - i])
          
-         for j in range (1, j):
-            if (self.DPTs[i][j].LRU > self.DPTs[i][j].LRU):
+         for b in range (0, i+1):
+            delta_seqence.append(raw_delta_sequence[b + self.n - 1 - i])
+         
+         for j in range (1, self.m):
+            if (self.DPTs[i][j].LRU > self.DPTs[i][victem].LRU):
                victem = j
                
          self.DPTs[i][victem] = DPTEntry(next_delta, delta_seqence)
                   
    #This function finds the next delta
-   def __find_next_delta(self): 
+   def __find_next_delta(self, delta_seq): 
       
       finds = []
       
-      for ib in range (self.n-1, -1): #Itterate over DPTs
-         i = self.n - ib - 1 #i -> n-1, n-2, n-3 ... 0
+      print(f"last delats: {self.last_deltas}")
+      
+      for ib in range (0, self.n): #Itterate over DPTs
+         i = self.n - ib - 1 #i -> n-1, n-2, n-3 ... 0,    3, 2, 1, 0
          found = True
          
          for j in range (0, self.m): #Itterate over #DPTS entires
-            
-            #next_delta = self.DPTs[i][j].next_delta
-            
-            for k in range(0, i+1): #Itterate over deltas in DPTS entire
+            for k in range(0, i+1): #Itterate over deltas in DPTS entire k -> 0,1,2,3 : 0,1,2 : 0,1 : 0
                
                #i is the table number, there are i+1 entries
                
@@ -78,66 +92,67 @@ class Prefetcher:
                #list 2: ib = 2, i = 1, k = [0,1,2]    offset_k = [1, 2, 3]
                #list 3: ib = 3, i = 0, k = [0,1,2,3]  offset_k = [0, 1, 2, 3]
                
-               #offset = self.n-i+1
-               
-               if (self.DPTs[i][j].delatas[k] != self.last_deltas[k+i]):
+               #print(f"i: {i}, ib: {ib}, j: {j}, k: {k}, offset_k: {k+ib}")
+               #print(self.DPTs[i][j].delatas)
+               print(f"entry deltas: {self.DPTs[i][j].delatas}")
+               if (self.DPTs[i][j].delatas[k] != delta_seq[k+ib]):
                   found = False
                   
-            if (found): 
+            if found: 
+               print("found a match!")
                finds.append((i, j)) 
-               break      
+               break  
+            
+         print("///")    
       
       #all delta seqences that match are now in the found list
-      for i, j in finds:
-         for k in range (0, self.m):
-            self.DPTs[i][k].LRU += 1
       
-            #return True, next_delta
-            #self.DPTs[i][j].LRU = 0 #Update Lru
-            
-         self.DPTs[i][j].LRU = 0
-         
       if (len(finds) == 0):
          return False, 0
       
+      self.__uppdate_LRU(finds)
+      
       i, j = finds[0]
-      return True, self.DPTs[i][j]
+      return True, self.DPTs[i][j].prediction
       
             
    def prefetch(self, adress):
-      
-      if (self.last_adress == None):
+      #Block first accses
+      if (self.last_adress is None):
          self.last_adress = adress
          return False, 0
       
       new_delta = adress - self.last_adress
-      
-      tmp = self.last_deltas
-      
-      print(len(self.last_deltas))
+      new_delta_seq = [0] * self.n
       
       for i in range (0, self.n-1):
-         self.last_deltas[i] = tmp[i+1]
+         new_delta_seq[i] = self.last_deltas[i+1]
    
-      self.last_deltas[self.n-1] = new_delta
+      new_delta_seq[self.n-1] = new_delta
       
-      valid, next_delta  = self.__find_next_delta()
+      print(f"last deltas: {new_delta_seq}")
+      print(f"old deltas:  {self.last_deltas }")
       
-      self.__update_deltas(tmp, new_delta)
-   
-      #Uppdate deltas
+      self.__update_deltas(self.last_deltas, new_delta)
       
-      if (not valid):
+      valid, next_delta  = self.__find_next_delta(new_delta_seq)
+      
+      self.last_deltas = new_delta_seq
+      self.last_adress =  adress
+      
+      if (not valid or next_delta is None):
          print("Nothing to prefetch, do dsiplacment insted!")
          return False, 0
-      
-                   
-      self.last_adress =  adress
       return True, next_delta + adress
    
 if __name__ == '__main__':
-   prefetcher = Prefetcher(4, 64)
+   prefetcher = Prefetcher(4, 4)
    found, next_delta = prefetcher.prefetch(1)
    found, next_delta = prefetcher.prefetch(2)
    found, next_delta = prefetcher.prefetch(3)
-   print(next_delta)
+   found, next_delta = prefetcher.prefetch(1)
+   found, next_delta = prefetcher.prefetch(2)
+   found, next_delta = prefetcher.prefetch(3)
+
+   if found:
+      print(next_delta)
