4ed8d3a6856e2a6195ef5853c4c90786946d7a84
Zsodrax <zsoltas@stud.ntnu.no>
2023-03-12 17:50:24 +0100

    first merge

--- VLDP_prefetcher.c
+++ VLDP_prefetcher.c
@@ -1,7 +1,14 @@
+#include "VLDP_prefetcher.h"
+
 int last_adress = -1;
 int last_deltas[n];
 int new_delta_seq[n];
 struct DPTEntry DPTs[n][m];
+
+unsigned int calculate_opt_adress(unsigned int addr, int delta);
+
+//create global direct mapped OPT cache table
+struct opt_table OPT_prefetch[opt_entry_num];
 
 void update_LRU(struct entry finds[]){
    for(int i = 0; i < n; i++){
@@ -130,6 +137,76 @@ struct prediction prefetch(int adress){
    pred.value = next_delta_prediction.value + adress;
 
    return pred;
+}
+
+unsigned int calculate_opt_adress(unsigned int addr, int delta)
+{
+    //delta unused
+    unsigned int calced_addr;
+
+    static unsigned char block_access = 0;
+
+    static unsigned int previous_tag = 0;
+    static long previous_addr = 0;
+    long current_addr = addr;
+    int delta_t; 
+
+    //calculate tag
+    unsigned int tmp_Tag = addr >> (int)log2(BLOCK_SIZE);
+    tmp_Tag = tmp_Tag >> (int)log2(opt_entry_num);
+
+    //first access of the page
+    if(tmp_Tag != previous_tag)
+    {
+        //check validity
+        if (OPT_prefetch[tmp_Tag].accuracy == OPT_ENTRY_ACCURATE)
+        {
+            calced_addr = OPT_prefetch[tmp_Tag].delta;
+        }
+        else
+        {
+            //Do nothing?
+            //Or return delta = 1? (Next Line)
+        }
+        block_access = 1;
+        
+    }
+    //Second access of the same page
+    else if( block_access == 1)
+    {
+        //calculate delta
+        delta_t = current_addr - previous_addr;
+        //printf("Actual delta: %d\n", delta_t);
+
+        //matching delta => good prediction
+        if (delta_t == OPT_prefetch[tmp_Tag].delta)
+        {
+            OPT_prefetch[tmp_Tag].accuracy = 1;
+            calced_addr = OPT_prefetch[tmp_Tag].delta;
+        }
+        //the stored mismatches with the current delta
+        else
+        {
+            if (OPT_prefetch[tmp_Tag].accuracy == 0)
+                OPT_prefetch[tmp_Tag].delta = delta_t;
+
+            OPT_prefetch[tmp_Tag].accuracy = 0;
+        }
+
+        block_access++;
+    }
+    //3 and 3+ accesses of the same page
+    else
+    {
+        //Do nothing
+    }
+    
+    //Save current datas 
+    previous_addr = current_addr;
+    previous_tag = tmp_Tag;
+
+    //return prefetchable addr(OFFSET!)
+    return calced_addr;
 }
 
 int main() {
--- VLDP_prefetcher.h
+++ VLDP_prefetcher.h
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>    
+
+//in bytes
+#define BLOCK_SIZE              131 072u
+#define MAIN_MEMORY_SIZE        8 388 608u
+#define opt_entry_num           MAIN_MEMORY_SIZE/BLOCK_SIZE //=> 64
+
+#define OPT_ENTRY_ACCURATE      1u
+#define OPT_ENTRY_INACCURATE    0u
+
 #define n 4
 #define m 8
 #define p 64
@@ -18,4 +31,11 @@ struct entry {
 struct prediction {
   int valid;
   int value;
+};
+
+struct opt_table
+{
+    unsigned int Tag;
+    int delta;
+    unsigned char accuracy;
 };
